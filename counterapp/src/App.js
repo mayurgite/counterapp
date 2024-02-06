a465598ea37b633cd1749b9db9343239e208b252
mayurgite.it@gmail.com <mayurgite.it@gmail.com>
2024-02-06 22:00:15 +0530

    test

---
+++ counterapp/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
